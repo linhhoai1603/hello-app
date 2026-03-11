eb62fbd54163b2a9de594e406d8891cf25941b09
Huynh Linh Hoai <22130084@st.hcmuaf.edu.vn>
2026-03-11 19:07:42 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  if (req.url === '/api/hello') {
+    res.end(JSON.stringify({ message: "Hello từ API!", status: "Success" }));
+  } else {
+    res.end(JSON.stringify({ message: "Welcome to Jenkins CI/CD", version: "1.0" }));
+  }
+});
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
